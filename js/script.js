296b9574f420d3542701c56a4dbdec959e4d2cca
Nguyễn Phương <88928922+NguyenPhuong21@users.noreply.github.com>
2021-09-22 12:24:49 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+let count = 0;
+const value =document.getElementById("value");
+const btns =document.querySelectorAll(".btn");
+btns.forEach(function (btn){
+    btn.addEventListener("click",function(e){
+        const style =e.currentTarget.classList;
+        if(style.contains("decrease")){
+            if(count>0){
+                count--;
+            }
+        }
+        else if (style.contains("increase")){
+            count++;
+        }
+        else if(style.contains("reset")){
+            count =0;
+        }
+        
+        value.textContent =count;
+    });
+});
 document.getElementById('btnScrollTop')
     .addEventListener('click', ()=>{
         window.scrollTo({
